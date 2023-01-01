5680d1ffe752582499e796b7bfae737a12f88a0f
Oumaima-BenElhajAli <oumaima.benelhadjali@gmail.com>
2023-01-01 17:43:14 +0100

    php app for aws

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+
+define('DB_HOST', 'project-db.cgknk86hjw1d.us-east-1.rds.amazonaws.com');
+define('DB_NAME', 'db_admin');
+define('DB_USER', 'admin');
+define('DB_PASS', 'RDSproject2022');
